b2a876a653482fb54f421dc79d8f4b5ec5573666
VC920 <3359924317@qq.com>
2026-05-13 16:21:40 +0800

    Update dynamic_array.h

--- dynamic_array.h
+++ dynamic_array.h
@@ -1,11 +1,19 @@
 #ifndef DYNAMIC_ARRAY_H
 #define DYNAMIC_ARRAY_H
 
+/* typedef struct { */
+/*     int *items; */
+/*     size_t count; */
+/*     size_t capacity; */
+/* } DA; */
+
+#define DA_INIT_CAPACITY 256
+
 #define da_reserve(da, expected_capacity)				\
     do{									\
 	if (expected_capacity > da.capacity) {				\
 	    if (da.capacity == 0) {					\
-		da.capacity = 256;					\
+		da.capacity = DA_INIT_CAPACITY;					\
 	    }								\
 	    while (expected_capacity > da.capacity) {			\
 		da.capacity *= 2;					\
@@ -40,47 +48,4 @@
 	da.items[j] = da.items[--da.count];	\
     } while (0)					\
 
-/* typedef struct { */
-/*     int *items; */
-/*     size_t count; */
-/*     size_t capacity; */
-/* } Numbers; */
-
-/* int main(void) */
-/* { */
-/*     Numbers da = { 0 }; */
-/*     da_resize(da, 500); */
-/*     da_reserve(da, 1000); */
-
-/*     printf("da count: %zu\n", da.count); */
-    
-/*     for (int x = 0; x < 500; x++) { */
-/* 	da.items[x] = x; */
-/*     } */
-
-/*     for (int x = 0; x < 500; x++) { */
-/* 	da_append(da, x); */
-/*     } */
-
-/*     for (int x = 0; x < 500; x++) { */
-/* 	int a = da_pop(da); */
-/*     } */
-
-/*     printf("da count: %zu\n", da.count); */
-/*     printf("da capacity: %zu\n", da.capacity); */
-    
-/*     printf("da first: %d\n", da_first(da)); */
-/*     printf("da last: %d\n", da_last(da)); */
-
-/*     da_remove_unordered(da, 10); */
-/*     printf("da first: %d\n", da_first(da)); */
-/*     printf("da last: %d\n", da_last(da)); */
-    
-/*     for (size_t i = 0; i < da.count; i++) { */
-/* 	printf("%d\n", da.items[i]); */
-/*     } */
-
-/*     da_free(da); */
-/*     return 0; */
-/* } */
 #endif // DYNAMIC_ARRAY_H
